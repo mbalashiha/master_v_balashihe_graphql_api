0ce075280998327c92dcbb78e3527d48f2288743
toylentoy <lentoytoy@gmail.com>
2023-11-11 00:03:04 +0300

    prittier switch for code formating added: only markup and tsx (jsx)

--- src/utils/prettier-react.ts
+++ src/utils/prettier-react.ts
@@ -10,9 +10,23 @@ export default async function prettierReact(
       textContent: string;
       language: string;
     } = req.body; //(req as any).rawBody || req.body.toString("utf8");
+    const { language } = value;
+    let parser: string = "";
+    switch (language) {
+      case "tsx":
+      case "jsx":
+        parser = "babel";
+        break;
+      case "markup":
+        parser = "html";
+        break;
+      default:
+        parser = "babel";
+        break;
+    }
     const newTextContent = await prettier.format(value.textContent.trim(), {
       semi: true,
-      parser: "babel",
+      parser,
     });
     return res.json({
       textContent: newTextContent
