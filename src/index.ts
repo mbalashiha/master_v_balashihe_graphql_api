43a83f2b387cac1c7d9ebbb612a2bae87f245f65
toylentoy <lentoytoy@gmail.com>
2023-12-10 01:25:26 +0300

    image parser auto timeout reduced to 3 seconds from start

--- src/index.ts
+++ src/index.ts
@@ -214,7 +214,7 @@ app.use(
   })
 );
 app.listen(parseInt(LISTENING_PORT), "127.0.0.1", () => {
-  setTimeout(() => parseImagesToDB(), 15000);
+  setTimeout(() => parseImagesToDB(), 3000);
   // `Running a GraphQL API server at http://localhost:${LISTENING_PORT}/graphql/api`
 });
 export default app;
